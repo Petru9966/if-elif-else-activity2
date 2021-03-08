ac2fc03063e31a725c673c3542e8f7e1dff59de3
repl.it user <replituser@example.com>
2021-03-08 06:33:22 +0000

    Multiple elif added

---
+++ main.py
@@ -0,0 +1,13 @@
+#Determining the type of entity
+print("What type of entity are you?")
+entity=input()
+if (entity=="Human"):
+  print("You are human!")
+elif(entity=="Robot"):
+  print("You are a robot!")
+elif(entity=="Animal"):
+  print("You are an animal!")
+else:
+  print("I don't know what you are!")
+  
+
